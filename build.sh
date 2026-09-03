448d3b798ed752e60e27381e38a0b24eeb3f11b7
Sander <sander.vandenkerkhof@kuleuven.be>
2026-09-03 16:43:59 +0200

    Updated build script

--- build.sh
+++ build.sh
@@ -10,6 +10,10 @@ mkdir build-release
 cmake -S . -B build-debug -G Ninja -DCMAKE_BUILD_TYPE=Debug
 cmake --build build-debug --target goat --verbose
 cmake --install build-debug --prefix ./executables/Debug  --verbose
-#cmake -S . -B build-release -G Ninja -DCMAKE_BUILD_TYPE=Release
-#cmake --build build-release 
-#cmake --install build-release --prefix ./executables/Release
+cmake -S . -B build-release -G Ninja -DCMAKE_BUILD_TYPE=Release
+cmake --build build-release 
+cmake --install build-release --prefix ./executables/Release
+
+# Construct symlinks to executables
+ln -sfn ./executables/Debug/goat goat_debug.exe
+ln -sfn ./executables/Release/goat goat.exe
